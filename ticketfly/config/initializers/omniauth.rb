6da07fc35a2f18d687309cce225f22b1c71c85f2
Danny Kulas <danny.kulas1@gmail.com>
2015-01-26 16:42:23 -0500

    added controllers and updated omniauth.rb file in config folder

--- ticketfly/config/initializers/omniauth.rb
+++ ticketfly/config/initializers/omniauth.rb
@@ -1,5 +1,12 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "fqvCypetw2ZgbUFCaaGUw6qlt", "gPJjbjPNeSTJys7GtC0d24RnjDn2sQZSIRr49Hzkk1KeRRMM5V"
+  provider :twitter, "fqvCypetw2ZgbUFCaaGUw6qlt", "gPJjbjPNeSTJys7GtC0d24RnjDn2sQZSIRr49Hzkk1KeRRMM5V", 
+  {
+  	:secure_image_url => "true",
+  	:image_size => "normal",
+  	:authorize_params => {
+  		:force_login => "true"
+  	}
+  }
 end
