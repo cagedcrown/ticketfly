4c6b5c8e40918fc2a5a91e674824d9da57a0b11e
Danny Kulas <danny.kulas1@gmail.com>
2015-01-26 17:04:00 -0500

    updated migrations and created folders for views

--- ticketfly/db/migrate/20150126205628_create_comments.rb
+++ ticketfly/db/migrate/20150126205628_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.integer :user_id
       t.integer :venue_id
+      t.string :body
 
       t.timestamps null: false
     end
--- ticketfly/db/schema.rb
+++ ticketfly/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150126210243) do
+ActiveRecord::Schema.define(version: 20150126214831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,12 +21,15 @@ ActiveRecord::Schema.define(version: 20150126210243) do
     t.integer  "venue_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "body"
   end
 
   create_table "users", force: :cascade do |t|
     t.string   "twitter_handle"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   create_table "venues", force: :cascade do |t|
