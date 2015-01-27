bd42ccc6d7eed2f97cfed261f975867efc4c2424
Danny Kulas <danny.kulas1@gmail.com>
2015-01-26 22:10:49 -0500

    added dependent destroy to user model associations

--- ticketfly/app/models/user.rb
+++ ticketfly/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-	has_many :votes
-	has_many :comments
+	has_many :votes, dependent: :destroy
+	has_many :comments, dependent: :destroy
 
 	def self.from_omniauth(auth)
 		where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
