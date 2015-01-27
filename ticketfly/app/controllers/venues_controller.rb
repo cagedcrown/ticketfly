55793c53034784f9bf244083d2c695ced9fa40c7
Danny Kulas <danny.kulas1@gmail.com>
2015-01-27 15:47:32 -0500

    updated venues controller

--- ticketfly/app/controllers/venues_controller.rb
+++ ticketfly/app/controllers/venues_controller.rb
@@ -8,19 +8,5 @@ class VenuesController < ApplicationController
 
   def show
   end
-
-  def new
-  end
-
-  def edit
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def destroy
-  end
+  
 end
