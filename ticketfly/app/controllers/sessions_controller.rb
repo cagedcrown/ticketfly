86d07326d535341928e07178bd37e0940f8b0893
Danny Kulas <danny.kulas1@gmail.com>
2015-01-27 10:48:58 -0500

    updated sessions controller and user model and added name to users table

--- ticketfly/app/controllers/sessions_controller.rb
+++ ticketfly/app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
 	def create
 		user = User.from_omniauth(env["omniauth.auth"])
-		session[:user_id] = user.user_id
+		session[:user] = user.id
 		redirect_to root_url, notice: "Signed in!"
 	end
 
 	def destroy
-		session[:user_id] = nil
+		session[:user] = nil
 		redirect_to root_url, notice: "Signed out!"
 	end
 end
--- ticketfly/app/models/user.rb
+++ ticketfly/app/models/user.rb
@@ -3,7 +3,12 @@ class User < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 
 	def self.from_omniauth(auth)
-		where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+			user.provider = auth.provider
+			user.uid = auth.uid
+			user.name = auth.info.name
+			user.save
+		end
 	end
 
 	def self.create_from_omniauth(auth)
