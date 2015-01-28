2dd0b787a8f43fca17e5b198c340264a834c2642
Danny Kulas <danny.kulas1@gmail.com>
2015-01-28 14:32:45 -0500

    udpated comments crud and views and application layout deprecated action

--- ticketfly/app/controllers/comments_controller.rb
+++ ticketfly/app/controllers/comments_controller.rb
@@ -16,15 +16,10 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(comment_params)
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to root_path, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    @user = current_user
+    @comment.user_name = @user.name
+    if @comment.save
+      redirect_to root_path
     end
   end
 
@@ -51,7 +46,7 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to "/", notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to "/", notice: 'Comments was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
