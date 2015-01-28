2e9a1ac50d7737e91045dbf850875642316aec69
Danny Kulas <danny.kulas1@gmail.com>
2015-01-28 09:25:42 -0500

    updated comments migrations and controller and form

--- ticketfly/db/schema.rb
+++ ticketfly/db/schema.rb
@@ -11,17 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127215409) do
+ActiveRecord::Schema.define(version: 20150128141800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "venue_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "body"
+    t.string   "title"
+    t.string   "venue_name"
+    t.string   "user_name"
   end
 
   create_table "rs_evaluations", force: :cascade do |t|
