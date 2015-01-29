d279a2c0218c95d2290d60527fe58fc2b89ef8ab
Danny Kulas <danny.kulas1@gmail.com>
2015-01-29 13:11:01 -0500

    removed un-needed commented-out js

--- ticketfly/app/assets/javascripts/application.js
+++ ticketfly/app/assets/javascripts/application.js
@@ -57,32 +57,3 @@ function displayResults( response, input ){
 });
 }
 //___________________________________________________________________________
-
-
-// $.getJSON("http://api.songkick.com/api/3.0/search/venues.json?query=" + query + "&apikey=" + apikey + "&jsoncallback=?", function(data){
-//  var venues = data['resultsPage']['results']['venue'];
-//  for (var i=0;i < venues.length; i++) {
-//    $("#venues").append('<div class="displayName"><li>' + venues[i]['displayName']+'</li></div>');
-//  }
-// });
-
-// function searchCallback(data) {
-//  // result.append('Found ' + data.total + ' results for ' + query);
-//  var venues = data.resultsPage;
-//  $.each(venues, function(index, venue) {
-//   result.append(venue.results.venue.displayName);
-// });
-
-//  $(".submit_buttons").on("submit",function(event){
-//   event.preventDefault();
-//   // var img_src = this.nextSibling.nextSibling.getAttribute('src');
-//   // var movie_id = this.firstChild.getAttribute("id");
-//   // var img_id = $(this).closest("img");
-//   // $.ajax({
-//   //   type: "POST",
-//   //   url: "/venues/",
-//   //   data: {venue_id: venue_id,
-//   //     img_src: img_src
-//   //   }
-// });
-// };
